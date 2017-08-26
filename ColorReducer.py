0336a21ae19b96e799a56ff05c36f5a6554c85c2
Evan Kivolowitz <ekivolowitz@wisc.edu>
2017-08-26 17:11:27 -0500

    Commenting

--- ColorReducer.py
+++ ColorReducer.py
@@ -1,19 +1,26 @@
-# from Colors import PRIMARY_COLORS as colors
-# from Colors import FIVE_SHADES_OF_RED as colors
+import sys
 from Colors import TEN_COLORS as colors
 from PIL import Image
 from math import sqrt
 from matplotlib.colors import to_rgba_array
-# from PIL import Image
-# print(dir(Image))
 
+# Used for debugging.
 hex_value = True
 
-
-im = Image.open('test-image.jpg')
+try:
+    im = Image.open(sys.argv[1])
+except:
+    raise("Error: cannot open image")
 px = im.load()
 
 if hex_value:
+    # This section of code iterates over the every pixel in the image,
+    # and converts its color to an rgb tuple. It will then calculate the 
+    # euclidean distance (https://en.wikipedia.org/wiki/Euclidean_distance)
+    # of all of the colors and set the color of the pixel to be the color with the 
+    # shortest distance. I'm not knowledgeable on color theory, but this is the best
+    # way to reduce colors that I could think of. As I learn more, I will create a less
+    # intensive, more efficient algorithm.
     for x in range(im.size[1]):
         for y in range(im.size[0]):
             minColorDistance = -1
@@ -35,6 +42,8 @@ if hex_value:
             px[x,y] = (int(minColor[0]), int(minColor[1]), int(minColor[2]))
 
 else:
+    # Same concept as in the if clause, except the color coming in is already in rgb form.
+    # I should probably turn this into a function and then just call it from above. 
     for x in range(im.size[1]):
         for y in range(im.size[0]):
             minColorDistance = -1
